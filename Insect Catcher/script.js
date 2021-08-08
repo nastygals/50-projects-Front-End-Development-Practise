dd46fa7e95dbb609142651a9c47ac8275bb37910
BenziBox <65805942+nastygals@users.noreply.github.com>
2021-08-08 12:28:29 +0200

    Add files via upload

---
+++ Insect Catcher/script.js
@@ -0,0 +1,11 @@
+const mainPage = document.querySelector('.mainPage');
+const btn = document.querySelector('.submit');
+const buttonEl = document.createElement('button');
+const btn2 = document.querySelector('.songName');
+
+btn.addEventListener('click', () =>{
+    mainPage.classList.add('up');
+
+})
+
+
