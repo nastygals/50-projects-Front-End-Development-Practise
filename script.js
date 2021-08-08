755ffa4c4b595cad611217bbfc1c91718054ebdb
BenziBox <65805942+nastygals@users.noreply.github.com>
2021-08-08 12:08:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+ let image = document.querySelectorAll('img');
+
+ const addActiveClass = (e) =>{
+         removeActiveClass();
+         e.target.classList.addActiveClass('active')
+     }
+ 
+ const removeActiveClass = () =>{
+     image.forEach(i =>{
+         i.classList.remove('active');
+     })
+ }
+ 
+ image.forEach(i =>{
+     i.addEventListener('click', addActiveClass());
+ })
+ 
