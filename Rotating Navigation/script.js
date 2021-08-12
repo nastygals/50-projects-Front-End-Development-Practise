16b0fe66f835105c903453c2a574f5d65546f429
BenziBox <miduyiqi20021002@icloud.com>
2021-08-12 12:00:44 +0200

    finished ! to review in later days

--- Rotating Navigation/script.js
+++ Rotating Navigation/script.js
@@ -2,6 +2,7 @@
 const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
+const navBar = document.querySelector('nav');
 
 /*open on click transform the nav bar to the right so anticlockwise so negative degree
 
@@ -10,9 +11,11 @@ opposite for close
 
 const addClass =()=>{
     container.classList.add('show-nav');
+    navBar.classList.add('nav-slide'); 
 }
 const removeClass = () =>{
     container.classList.remove('show-nav');
+    navBar.classList.remove('nav-slide');
 }
 
 open.addEventListener('click', addClass);
