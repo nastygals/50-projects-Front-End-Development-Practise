7ab12de853ac01504aa2aac9982320d4b4921ffa
BenziBox <miduyiqi20021002@icloud.com>
2021-08-12 13:16:50 +0200

    correct the transition property

--- Hidden Search/script.js
+++ Hidden Search/script.js
@@ -1,1 +1,13 @@
+const searchContainer = document.querySelector('.search');
+const selectI = document.querySelector('.click');
 
+// add class to button so that when clicked it shows the
+const addOrRemove = () =>{
+    if(searchContainer.classList =='search'){
+        searchContainer.classList.add('show-bar');
+    } else{
+        searchContainer.classList.remove('show-bar');
+    }
+}
+
+selectI.addEventListener('click', addOrRemove);
