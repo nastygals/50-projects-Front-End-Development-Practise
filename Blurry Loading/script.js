19b35b038ab2708473d65daaddb9dfa7eda1d9f8
BenziBox <miduyiqi20021002@icloud.com>
2021-08-14 13:47:44 +0200

    Finished project

--- Blurry Loading/script.js
+++ Blurry Loading/script.js
@@ -1,25 +1,21 @@
-const loadText = document.querySelector('.loading-text');
 const bg = document.querySelector('.bg');
+const text = document.querySelector('.loading-text');
 
-let load= 0;
+let load = 0;
+let percent = 10;
 
-const scale = (num, in_min, in_max, out_min, out_max) => {
-    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-  }
+let interval = setInterval(blurring,30);
 
-  let int = setInterval(blurring,30);
-
-function blurring (){
-    load++
-    if(load > 99){
-        clearInterval(int);
-    } 
-
-    loadText.innerHTML = `${load} %`
-    loadText.getElementsByClassName.opacity = scale(load,0,100,1,0);
-    bg.style.filter = `blur(${scale(load,0,100,30,0)}px)`
+function blurring(){
+    load++;
+    percent -= 0.1;
+    if(load > 100){
+        clearInterval(interval);
+        text.style.opacity = 0;
+    }
+    text.innerHTML = `${load} %`
+    bg.style.filter = `blur(${percent}rem)`
 }
 
 
 
-// function that generates numbers from 0 to 1 at intervals of 0.1
