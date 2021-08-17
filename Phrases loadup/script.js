bd2fdfb4dcd83c0f7ebc00974ccf681912f56586
BenziBox <miduyiqi20021002@icloud.com>
2021-08-17 16:34:24 +0200

    fix last bugs for the looping and blinking animation

---
+++ Phrases loadup/script.js
@@ -0,0 +1,54 @@
+
+const injectText = ['This','animation','is','really','cool'];
+const display = document.querySelector('.displayText2');
+const cursor = document.querySelector('.cursor');
+
+let charIndex = 0; 
+let arrayTextIndex = 0;
+
+const typeTime = 200;
+const eraseTime = 100;
+const newWordDelay = 2000;
+
+function type(){
+    if(arrayTextIndex == injectText.length){
+        arrayTextIndex = 0; 
+        charIndex = 0
+        setTimeout(type,newWordDelay);
+    }
+    if (charIndex < injectText[arrayTextIndex].length){
+        if(!cursor.classList.contains('typing')){
+            cursor.classList.add('typing');
+        }
+        display.textContent += injectText[arrayTextIndex].charAt(charIndex);
+        charIndex++;
+
+        setTimeout(type,typeTime);
+    } else{
+        setTimeout(erase,eraseTime);
+    }
+}
+
+function erase(){
+    if(charIndex > 0){
+        if(cursor.classList.contains('typing')){
+            cursor.classList.remove('typing');
+        }
+        display.textContent = injectText[arrayTextIndex].substring(0,charIndex-1);
+        charIndex--;
+            setTimeout(erase,eraseTime);
+    }else{
+        if(arrayTextIndex >= injectText.length){
+            arrayTextIndex = 0; 
+            charIndex = 0;
+            setTimeout(type,typeTime + 1100);
+        } else{
+            arrayTextIndex++;
+            setTimeout(type,newWordDelay);
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    if(injectText.length)setTimeout(type,newWordDelay + 250);
+});
